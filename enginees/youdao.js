fabc99ac690344f2ab30c8790ea308d06fff6c67
jasonhuang <1248394650@qq.com>
2020-11-23 17:38:16 +0800

    feat: add youdao translate

---
+++ enginees/youdao.js
@@ -0,0 +1,97 @@
+const http = require('http');
+const chalk = require('chalk');
+
+module.exports = (query, config) => {
+  return new Promise((resolve, reject) => {
+    const pos = Math.floor(Math.random() * config.youdao.length);
+    const key = config.youdao[pos].key;
+    const keyfrom = config.youdao[pos].keyfrom;
+    const q = Array.isArray(query) ? query.join(' ') : query;
+    const data = {
+      key,
+      keyfrom,
+      type: 'data',
+      doctype: 'json',
+      version: '1.1',
+      q
+    }
+
+    const errorCode = {
+      0: '正常',
+      20: '要翻译的文本过长',
+      30: '无法进行有效的翻译',
+      40: '不支持的语言类型',
+      50: '无效的key',
+      60: '无词典结果,仅在获取词典结果生效'
+    }
+
+    let url = 'http://fanyi.youdao.com/openapi.do?';
+
+    for (let item in data) {
+      url += `${item}=${data[item]}&`;
+    }
+
+    url = encodeURI(url);
+
+    http.get(url, (res) => {
+      var resData = "";
+      res.on("data",function(data){
+        resData += data;
+      });
+      res.on("end", function() {
+        resData = JSON.parse(resData);
+        let str = '';
+        if (resData.errorCode === 0) {
+          str += `\r\n  ${chalk.green('~')} ${chalk.cyan(resData.query)}`;
+
+          if ('basic' in resData) {
+            let basic = resData.basic
+            if ('phonetic' in basic) {
+              str +=  chalk.magenta(` - [${basic.phonetic}]`);
+            }
+            if ('us-phonetic' in basic) {
+              str += chalk.magenta(`   美[${basic['us-phonetic']}]`);
+            }
+            if ('uk-phonetic' in basic) {
+              str += chalk.magenta(`   英[${basic['uk-phonetic']}]`);
+            }
+            str += `\r\n  ${chalk.green('~')} ${chalk.cyan(resData.translation.join(','))}`;
+
+            if ('explains' in basic) {
+              str += `\r\n`;
+              for (let item of basic.explains) {
+                str += chalk.green(`\r\n   -  ${item}`);
+              }
+              str += `\r\n`;
+            }
+          } else {
+            str += `\r\n  ${chalk.green('~')} ${chalk.cyan(resData.translation.join(','))}`;
+          }
+
+
+          if ('web' in resData) {
+            let count = 1;
+            let regExp = new RegExp(`(${Array.isArray(query) ? query.join('|') : query})`, 'ig');
+            for (let item of resData.web) {
+              str += `\r\n   ${chalk.gray(count++)}. ${item.key.replace(regExp, chalk.yellow(`$&`))}`;
+              str += chalk.blue(`\r\n     ${item.value.join(',')} \r\n`);
+            }
+            str += `\r\n`;
+          }
+
+          console.log(str)
+          // console.log(resData);
+        } else {
+          console.log(errorCode[resData.errorCode]);
+        }
+        resolve(str)
+      });
+    })
+    .on('error', (e) => {
+      console.log(`Got error: ${e.message}`);
+      reject((e))
+    });
+
+  })
+
+}
